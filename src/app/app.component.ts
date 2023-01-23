be45827fc0d57e74d3fc077026ad20431c3b5125
kannanATexcelforte <kannan@excelforte.com>
2023-01-23 16:05:13 +0530

    23.01.2023
    one address has completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faLocationDot, faArrowRightLong, faCarSide } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DistancePresenter';
+  faLocationDot = faLocationDot;
+  faArrowRightLong = faArrowRightLong;
+  faCarSide = faCarSide;
 }
